7b65d76f495af487fdcb0452d80cda77009c66fe
Emilse <emilsearias@gmail.com>
2021-04-13 11:13:20 -0300

    se agrego la logica para agregar un producto a la api

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,18 +1,75 @@
 import React, { Fragment, useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Alert } from "react-bootstrap";
 
 const AgregarProducto = () => {
+  const URL = process.env.REACT_APP_API_URL;
+  console.log(URL);
   const [nombreProducto, setNombreProducto] = useState("");
   const [precioProducto, setPrecioProducto] = useState("");
   const [categoria, setCategoria] = useState("");
+  const [error, setError] = useState(false);
+
+  const cambiarCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    // validar los datos
+    if (
+      nombreProducto.trim() === "" ||
+      precioProducto.trim() === "" ||
+      categoria === ""
+    ) {
+      // mostrar un cartel de error;
+      setError(true);
+      return;
+    } else {
+      // enviar el producto a la api
+      console.log("aqui esta todo bien envio los datos");
+      setError(false);
+
+      // crear el objeto a enviar
+      const datos ={
+        nombreProducto,
+        precioProducto,
+        categoria
+      };
+      console.log(datos);
+
+      // enviar objeto a la api, operacion POST
+      try{
+        const parametros ={
+          method: "POST",
+          headers:{
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(datos)
+        };
+
+        // ejecutar la solicitud o request
+        const respuesta = await fetch(URL, parametros)
+        console.log(respuesta);
+
+      }catch(error){
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Fragment>
       <Container className="my-4">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <h1 className="my-4 text-center">Agregar nuevo producto</h1>
           <Form.Group>
-            <Form.Label>Nombre del producto</Form.Label>
+            {error === true ? (
+              <Alert variant={"danger"}>
+                Todos los campos son obligatorios
+              </Alert>
+            ) : null}
+
+            <Form.Label>Nombre del producto *</Form.Label>
             <Form.Control
               type="text"
               placeholder="Submarino"
@@ -20,50 +77,54 @@ const AgregarProducto = () => {
             ></Form.Control>
           </Form.Group>
           <Form.Group>
-            <Form.Label>Precio</Form.Label>
-            <Form.Control type="number" placeholder="$50" onChange={(e)=> setPrecioProducto(e.target.value)}></Form.Control>
+            <Form.Label>Precio *</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="$50"
+              onChange={(e) => setPrecioProducto(e.target.value)}
+            ></Form.Control>
           </Form.Group>
           <div className="text-center my-4">
-            <h3>Categoria</h3>
+            <h3>Categoria *</h3>
             <Form.Check
               inline
               type="radio"
               label="Bebida caliente"
               name="categoria"
-              value='bebida-caliente'
-              onChange={(e)=> setCategoria(e.target.value)}
+              value="bebida-caliente"
+              onChange={cambiarCategoria}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Bebida Fria"
               name="categoria"
-              value='bebida-fria'
-              onChange={(e)=> setCategoria(e.target.value)}
+              value="bebida-fria"
+              onChange={cambiarCategoria}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Sandwich"
               name="categoria"
-              value='sandwich'
-              onChange={(e)=> setCategoria(e.target.value)}
+              value="sandwich"
+              onChange={cambiarCategoria}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Dulce"
               name="categoria"
-              value='dulce'
-              onChange={(e)=> setCategoria(e.target.value)}
+              value="dulce"
+              onChange={cambiarCategoria}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Salado"
               name="categoria"
-              value='salado'
-              onChange={(e)=> setCategoria(e.target.value)}
+              value="salado"
+              onChange={cambiarCategoria}
             ></Form.Check>
           </div>
           <Button variant="danger" type="submit" className="w-100">
